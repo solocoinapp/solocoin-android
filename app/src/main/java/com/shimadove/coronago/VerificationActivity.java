1c31da906da81aba44a9c579dd43da4ad00583b9
Gabriel <gabrielmbaiorga@gmail.com>
2020-03-25 19:14:05 +0100

    add Json

--- app/src/main/java/com/shimadove/coronago/VerificationActivity.java
+++ app/src/main/java/com/shimadove/coronago/VerificationActivity.java
@@ -1,14 +1,75 @@
 package com.shimadove.coronago;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.shimadove.coronago.databinding.ActivityMainBinding;
+import com.shimadove.coronago.databinding.ActivityVerificationBinding;
 
 public class VerificationActivity extends AppCompatActivity {
+    private static final String TAG = "PhoneAuthActivity";
+
+    private static final String KEY_VERIFY_IN_PROGRESS = "key_verify_in_progress";
+
+    private static final int STATE_INITIALIZED = 1;
+    private static final int STATE_CODE_SENT = 2;
+    private static final int STATE_VERIFY_FAILED = 3;
+    private static final int STATE_VERIFY_SUCCESS = 4;
+    private static final int STATE_SIGNIN_FAILED = 5;
+    private static final int STATE_SIGNIN_SUCCESS = 6;
+
+    private boolean mVerificationInProgress = false;
+    private String mVerificationId;
+
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+
+    private EditText mPhoneNumberField;
+    private EditText mVerificationField;
+    TextView mCountyList;
+    private ProgressBar progressBar;
+
+    private String countryCode, enteredCode;
+    private Button mStartButton;
+    private boolean codeSent = false;
+    private ImageView imageBanner;
+
+    private FirebaseAuth mAuth;
+    ActivityVerificationBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_verification);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_verification);
+        if (savedInstanceState != null) {
+            onRestoreInstanceState(savedInstanceState);
+        }
+        initView();
+        initMode();
+    }
+
+    private void initMode() {
+
+    }
+
+    private void initView() {
+
+    }
+
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mVerificationInProgress = savedInstanceState.getBoolean(KEY_VERIFY_IN_PROGRESS);
     }
 }
