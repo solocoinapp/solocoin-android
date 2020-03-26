95126309a9fb0332a4792409d480e5aaf605f658
aayu24 <patni170101001@iitg.ac.in>
2020-03-27 03:12:55 +0530

    Linked all the intro pages and auth page.

--- app/src/main/java/com/shimadove/coronago/Main2Activity.java
+++ app/src/main/java/com/shimadove/coronago/Main2Activity.java
@@ -4,17 +4,43 @@ package com.shimadove.coronago;
 
 //import android.os.Bundle;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.shimadove.coronago.databinding.ActivityMain2Binding;
 
 public class Main2Activity extends AppCompatActivity {
-    private Button myButton;
+    ActivityMain2Binding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        binding= DataBindingUtil.setContentView(this, R.layout.activity_main2); //actually binds the view to binding
+        buttonClick();
+    }
 
+    private void buttonClick(){
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Main2Activity.this,VerificationActivity.class));
+            }
+        });
+        binding.button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Main2Activity.this,MainActivity.class));
+            }
+        });
+        binding.button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Main2Activity.this,Main3Activity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/shimadove/coronago/Main3Activity.java
+++ app/src/main/java/com/shimadove/coronago/Main3Activity.java
@@ -1,14 +1,40 @@
 package com.shimadove.coronago;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.shimadove.coronago.databinding.ActivityMain3Binding;
 
 public class Main3Activity extends AppCompatActivity {
-
+    ActivityMain3Binding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main3);
+        buttonClick();
+    }
+    private void buttonClick(){
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Main3Activity.this,VerificationActivity.class));
+            }
+        });
+        binding.button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Main3Activity.this,MainActivity.class));
+            }
+        });
+        binding.button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Main3Activity.this,Main2Activity.class));
+            }
+        });
     }
 }
