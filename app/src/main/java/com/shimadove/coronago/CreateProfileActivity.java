bef947fbdc0d28f0e0d55384ec7b7e01968e6ad3
Ken Choong <matchartproduction@gmail.com>
2020-03-27 15:32:18 +0800

    CreateProfileActivity UI

---
+++ app/src/main/java/com/shimadove/coronago/CreateProfileActivity.java
@@ -0,0 +1,69 @@
+package com.shimadove.coronago;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
+import timber.log.Timber;
+
+import android.os.Bundle;
+
+import com.shimadove.coronago.databinding.ActivityCreateProfileBinding;
+import com.shimadove.coronago.viewmodel.CreateProfileViewModel;
+
+public class CreateProfileActivity extends AppCompatActivity implements CreateProfileViewModel.CreateProfileInterface {
+
+    ActivityCreateProfileBinding binding;
+    CreateProfileViewModel viewModel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_profile);
+
+         binding = DataBindingUtil.setContentView(this,R.layout.activity_create_profile);
+         viewModel = new ViewModelProvider(this).get(CreateProfileViewModel.class);
+         binding.getRoot();
+         binding.setCreateProfileViewModel(viewModel);
+
+         viewModel.setCreateProfileInterface(this);
+    }
+
+    @Override
+    public void onCreateProfileSuccess() {
+
+    }
+
+    @Override
+    public void onCreateProfileFailed() {
+
+    }
+
+    @Override
+    public void onContinueClicked() {
+
+    }
+
+    @Override
+    public void onFacebookClicked() {
+        Timber.d("Facebook");
+    }
+
+    @Override
+    public void onGoogleClicked() {
+        Timber.d("Google");
+    }
+
+    @Override
+    public void onMaleClicked() {
+
+    }
+
+    @Override
+    public void onFemaleClicked() {
+
+    }
+
+    @Override
+    public void onSkip() {
+
+    }
+}
---
+++ app/src/main/java/com/shimadove/coronago/viewmodel/CreateProfileViewModel.java
@@ -0,0 +1,66 @@
+package com.shimadove.coronago.viewmodel;
+
+import android.app.Application;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.ViewModel;
+import timber.log.Timber;
+
+public class CreateProfileViewModel extends AndroidViewModel {
+
+    public String email,username,dob;
+
+    public CreateProfileViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public void setCreateProfileInterface(CreateProfileInterface signUpInterface) {
+        this.createProfileInterface = signUpInterface;
+    }
+
+    private CreateProfileInterface createProfileInterface;
+
+    public interface CreateProfileInterface{
+        void onCreateProfileSuccess();
+        void onCreateProfileFailed();
+        void onContinueClicked();
+        void onFacebookClicked();
+        void onGoogleClicked();
+        void onMaleClicked();
+        void onFemaleClicked();
+        void onSkip();
+    }
+
+    public void onMaleClicked(View view){
+        createProfileInterface.onMaleClicked();
+    }
+
+    public void onFemaleClicked(View view){
+        createProfileInterface.onFemaleClicked();
+    }
+
+    public void onLoginButtonClick(View view){
+        Timber.d("Login clicked");
+    }
+
+    public void onContinueButtonClick(View view){
+        Timber.d("Sign Up clicked");
+        createProfileInterface.onContinueClicked();
+    }
+
+    public void onFacebookClicked(View view){
+        Timber.d("Facebook clicked");
+        createProfileInterface.onFacebookClicked();
+    }
+
+    public void onGoogleClicked(View view){
+        Timber.d("Google clicked");
+        createProfileInterface.onGoogleClicked();
+    }
+
+    public void onSkip(View view){
+        createProfileInterface.onSkip();
+    }
+}
