eb6128ae4f1a63652d5ef496389efc4a7395628f
Ken Choong <matchartproduction@gmail.com>
2020-04-01 17:13:29 +0800

    Profile Fragment and Home Activity - Home Activity is Bottom Navigation view to host the 4 fragment - Profile Fragment UI

--- app/src/main/java/com/shimadove/coronago/ProfileFragment.java
+++ app/src/main/java/com/shimadove/coronago/ProfileFragment.java
@@ -2,8 +2,10 @@ package com.shimadove.coronago;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import timber.log.Timber;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,7 +15,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements View.OnClickListener {
 
     Toolbar toolbar;
     Spinner spinner;
@@ -24,39 +26,38 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        // Getting the toolbar with the Language spinner setup
-        toolbar = view.findViewById(R.id.profile_toolbar);
-        spinner = view.findViewById(R.id.language_select_spinner);
-
-        toolbar.setLogo(R.mipmap.logo);
-
-        preferences = getActivity().getPreferences(Context.MODE_PRIVATE);
-        preferencesEditor = preferences.edit();
-
-        // Managing spinner, default setting to appropriate language.
-        ArrayAdapter<String> languagesAdapter = new ArrayAdapter<String>(getActivity(), R.layout.profile_spinner_item, getResources().getStringArray(R.array.languages));
-        languagesAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-        spinner.setAdapter(languagesAdapter);
-        String language = preferences.getString("lang", "English");
-        int spinnerPosition = languagesAdapter.getPosition(language);
-        spinner.setSelection(spinnerPosition);
-
-
-
-        // Saving language info.
-        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                preferencesEditor.putString("lang", spinner.getSelectedItem().toString());
-                preferencesEditor.apply();
-            }
-        });
-
-        // Manage all button intents.
-
+        view.findViewById(R.id.permissionButton).setOnClickListener(this);
+        view.findViewById(R.id.profileButton).setOnClickListener(this);
+        view.findViewById(R.id.privacyPolicy).setOnClickListener(this);
+        view.findViewById(R.id.termConditionButton).setOnClickListener(this);
         return view;
     }
 
+    public static ProfileFragment newInstance() {
+        return new ProfileFragment();
+    }
+    
+    
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.permissionButton:
+                Timber.d("Permission stuff");
+                break;
+            case R.id.profileButton:
+                Timber.d("Edit profile stuff");
+                break;
+            case R.id.privacyPolicy:
+                Intent intent = new Intent(getActivity(),Policy.class);
+                startActivity(intent);
+                break;
+            case R.id.termConditionButton:
+                Intent intent1 = new Intent(getActivity(),ToS.class);
+                startActivity(intent1);
+                break;
+        }
+    }
 }
