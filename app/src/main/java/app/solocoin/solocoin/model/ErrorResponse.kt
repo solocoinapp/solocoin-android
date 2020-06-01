0922b248da71387384582e045b5d37cfbfde2371
saspg98 <sauravgupta97@gmail.com>
2020-06-01 14:12:41 +0530

    preparing release build

--- app/src/main/java/app/solocoin/solocoin/model/ErrorResponse.kt
+++ app/src/main/java/app/solocoin/solocoin/model/ErrorResponse.kt
@@ -1,5 +1,8 @@
 package app.solocoin.solocoin.model
 
+/**
+ * Created by Saurav Gupta on 29/05/2020
+ */
 data class ErrorResponse(
     var error: String? = null,
     var errors: ArrayList<String>? = null
